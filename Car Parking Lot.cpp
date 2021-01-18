15acff69fd85ef4cc14654a216d813b1d9ea1816
Vikrant Sharma <iamvikrant1@gmail.com>
2021-01-18 21:26:36 +0530

    Update Car Parking Lot.cpp

--- Car Parking Lot.cpp
+++ Car Parking Lot.cpp
@@ -1,8 +1,65 @@
-#include<iostream>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <numeric>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <fstream>
+#include <unordered_map>
+#define ll long long
+#include <list>
+
 using namespace std;
+
+// OOPS Concepts we have to use to design a Parking Lot System
+
+// Class
+// Objects
+// Encapsulation
+// Abstraction
+// Polymorphism
+// Inheritance
+// Dynamic Binding
+// Message Passing
+
+//Requirement Gathering
+/*
+ * Size:- 10k-30k Vehicles
+ * Entrance:- 4
+ * Exit:- 4
+ * Tickets on the Entrance
+ * Payment on the Exit
+ * Parking spot near available near the entrance
+ * Cannot exceed the 30k limit
+ * Type of Vehicles:-
+ * 1) MotoCycle
+ * 2) Car
+ * 3) Truck
+ * 4) Cycle
+ *
+ * Hourly rate per type of vehicles
+ * Payment option:-
+ * 1) Card
+ * 2) Cash
+ * 3) E-Wallet
+ *
+ * Monitoring System by Video surveillance
+ */
+
+//Design the Parking lot in such a way that we can reuse it with different places
+
+
 
 int main(){
 
 
+
+
     return 0;
 }
+
+
